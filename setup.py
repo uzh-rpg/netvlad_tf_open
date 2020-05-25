6da8978c4aaa894254e4190a477d67cf0b9da316
Tobias Fischer <info@tobiasfischer.info>
2020-05-25 13:36:16 +1000

    setuptools to install outside of catkin

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+try:
+    ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+    from catkin_pkg.python_setup import generate_distutils_setup
+    from distutils.core import setup
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['netvlad_tf'],
-    package_dir={'': 'python'},
-)
+    # fetch values from package.xml
+    setup_args = generate_distutils_setup(
+        packages=['netvlad_tf'],
+        package_dir={'': 'python'},
+    )
 
-setup(**setup_args)
+    setup(**setup_args)
+except ImportError:
+    from setuptools import setup
+    setup(
+        name='netvlad_tf',
+        packages=['netvlad_tf'],
+        package_dir={'': 'python'},
+        version='0.0.0'
+    )
+
